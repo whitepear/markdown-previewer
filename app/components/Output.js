5669dfec23dc16310bf35882963a4fa98db8cf01
John Doe <webdevnorth@gmail.com>
2016-08-11 15:17:18 +0100

    minor restructure of divs within Output.js

--- app/components/Output.js
+++ app/components/Output.js
@@ -3,7 +3,9 @@ var PropTypes = React.PropTypes;
 
 function Output (props) {	
 	return (
-		<div className="col-xs-12 col-sm-6" dangerouslySetInnerHTML={props.convertedMarkdown} />		
+		<div className="col-xs-12 col-sm-6">
+			<div dangerouslySetInnerHTML={props.convertedMarkdown}></div>
+		</div>		
 	)
 }
 
