67c472d9c205a61a24872532b7eae044a0e35b8f
John Doe <webdevnorth@gmail.com>
2016-08-11 20:30:23 +0100

    refactored state and Input to start with default markdown input

--- app/components/Input.js
+++ app/components/Input.js
@@ -4,14 +4,15 @@ var PropTypes = React.PropTypes;
 function Input (props) {
 	return (
 		<div className="col-xs-12 col-sm-6">
-			<textarea className="form-control" rows="22" name="input" onChange={props.onInput}>
-				
+			<textarea className="form-control" rows="22" name="markDownInput" value={props.rawText} onChange={props.onInput}>
+			
 			</textarea>
 		</div>
 	)
 }
 
 Input.propTypes = {
+	rawText: PropTypes.string.isRequired,
 	onInput: PropTypes.func.isRequired
 };
 
--- app/containers/MainContainer.js
+++ app/containers/MainContainer.js
@@ -10,21 +10,23 @@ marked.setOptions({
 var MainContainer = React.createClass({
 	getInitialState: function () {
 		return {
-			text: {
+			rawText: 'Heading\n=======\n\nSub-heading\n-----------\n \n### Another deeper heading\n \nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**, \n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\nThe rain---not the reign---in\nSpain.\n\n *[whitepear](https://github.com/whitepear)*',
+			markedText: {
 				__html: marked('Heading\n=======\n\nSub-heading\n-----------\n \n### Another deeper heading\n \nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**, \n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\nThe rain---not the reign---in\nSpain.\n\n *[whitepear](https://github.com/whitepear)*')
 			}
 		}
 	},
 	handleInput: function (e) {
 		this.setState({
-			text: {__html: marked(e.target.value)}
+			rawText: e.target.value,
+			markedText: {__html: marked(e.target.value)}
 		});		
 	},
 	render: function () {
 		return (
 	    <div className="row">
-	    	<Input onInput={this.handleInput} />
-	    	<Output convertedMarkdown={this.state.text} />
+	    	<Input onInput={this.handleInput} rawText={this.state.rawText} />
+	    	<Output convertedMarkdown={this.state.markedText} />
 	    </div>				
 		)
 	}
