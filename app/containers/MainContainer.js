0f94113630b5fcad4f0a415c7585d85f2421f8ca
John Doe <webdevnorth@gmail.com>
2016-08-11 19:26:58 +0100

    set initial state to produce markdown rules within output

--- app/containers/MainContainer.js
+++ app/containers/MainContainer.js
@@ -11,7 +11,7 @@ var MainContainer = React.createClass({
 	getInitialState: function () {
 		return {
 			text: {
-				__html: ''
+				__html: marked('Heading\n=======\n\nSub-heading\n-----------\n \n### Another deeper heading\n \nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**, \n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\nThe rain---not the reign---in\nSpain.\n\n *[whitepear](https://github.com/whitepear)*')
 			}
 		}
 	},
